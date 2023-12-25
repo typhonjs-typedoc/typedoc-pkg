a48e4b81582a6aeaa2750539e11a50fca9885704
typhonrt <support@typhonjs.io>
2023-12-24 17:55:58 -0800

    Initial config file support.

--- src/cli/functions.js
+++ src/cli/functions.js
@@ -1,10 +1,15 @@
-import fs                     from 'node:fs';
+import fs                  from 'node:fs';
+import { pathToFileURL }   from 'node:url';
 
-import path                   from 'upath';
+import { isFile }          from '@typhonjs-utils/file-util';
+import {
+   isIterable,
+   isObject }              from '@typhonjs-utils/object';
+import path                from 'upath';
 
-import { generateDocs }       from '../generator/index.js';
+import { generateDocs }    from '../generator/index.js';
 
-import { Logger }             from '#util';
+import { Logger }          from '#util';
 
 /**
  * Processes CLI options and invokes `generateDocs`.
@@ -15,9 +20,54 @@ import { Logger }             from '#util';
  */
 export async function generate(opts)
 {
-   const config = await processOptions(opts);
+   const configs = await processOptions(opts);
 
-   await generateDocs(config);
+   if (isIterable(configs))
+   {
+      for (const config of configs) { await generateDocs(config); }
+   }
+   else
+   {
+      await generateDocs(configs);
+   }
+}
+
+// Internal implementation -------------------------------------------------------------------------------------------
+
+/**
+ * @param {string}   filepath - Filepath of config.
+ *
+ * @returns {Promise<import('../generate').GenerateConfig | import('../generate').GenerateConfig[]>} Loaded config.
+ */
+async function loadConfig(filepath)
+{
+   const module = await import(pathToFileURL(filepath));
+
+   if (module.default === void 0) { exit(`The config does not have a default export: ${filepath}`); }
+
+   // Do some lite error checking on the provided config.
+   if (isObject(module.default))
+   {
+   }
+   else if (isIterable(module.default))
+   {
+      let i = 0;
+      for (const entry of module.default)
+      {
+         if (!isObject(entry))
+         {
+            exit(`The config file exports a list, but entry[${i}] is not an object: ${filepath}`);
+         }
+
+         i++;
+      }
+   }
+   else
+   {
+      exit(`The config file default export is not an object or iterable: ${filepath}`);
+   }
+
+   return module.default;
 }
 
 /**
@@ -25,16 +75,13 @@ export async function generate(opts)
  *
  * @returns {import('../generator').GenerateConfig} Converted CLI options to GenerateConfig.
  */
-function processOptions(opts)
+async function processOptions(opts)
 {
-   /**
-    * @type {import('../generator').GenerateConfig}
-    */
-   let config = {};
+   let logLevel;
 
    // logLevel -------------------------------------------------------------------------------------------------------
 
-   if (typeof opts?.loglevel === 'string')
+   if (typeof opts.loglevel === 'string')
    {
       if (!Logger.isValidLevel(opts.loglevel))
       {
@@ -42,8 +89,36 @@ function processOptions(opts)
       }
 
       Logger.logLevel = opts.loglevel;
-      config.logLevel = opts.loglevel;
+      logLevel = opts.loglevel;
    }
+
+   if (opts.config)
+   {
+      // return processConfigFile(opts, logLevel);
+      return processConfigFile(opts, logLevel);
+      // console.log(`!!!! Configs: `, configs)
+   }
+   else
+   {
+      return processConfigDefault(opts, logLevel);
+   }
+}
+
+/**
+ * Creates a GenerateConfig object directly from CLI options.
+ *
+ * @param {object}   opts - CLI options.
+ *
+ * @param {string}   logLevel - CLI parsed log level.
+ *
+ * @returns {GenerateConfig}
+ */
+function processConfigDefault(opts, logLevel)
+{
+   /**
+    * @type {import('../generator').GenerateConfig}
+    */
+   let config = { logLevel };
 
    // path -----------------------------------------------------------------------------------------------------------
 
@@ -90,6 +165,65 @@ function processOptions(opts)
    // tsconfigPath ---------------------------------------------------------------------------------------------------
 
    if (typeof opts.tsconfig === 'string') { config.tsconfigPath = opts.tsconfig; }
+
+   return config;
+}
+
+async function processConfigFile(opts)
+{
+   const dirname = path.dirname(process.cwd());
+
+   /**
+    * @type {import('../generator').GenerateConfig | Iterable<import('../generator').GenerateConfig>}
+    */
+   let config;
+
+   switch (typeof opts.config)
+   {
+      // Load default config.
+      case 'boolean':
+         if (!isFile('./typedoc-pkg.config.js') && !isFile('./typedoc-pkg.config.mjs'))
+         {
+            exit(`No default config file 'typedoc-pkg.config.[m]js' available in: ${dirname}`);
+         }
+
+         if (isFile('./typedoc-pkg.config.js'))
+         {
+            Logger.verbose(`Loading config from path: './typedoc-pkg.config.js'`);
+            config = await loadConfig(path.resolve('./typedoc-pkg.config.js'));
+         }
+         else if (isFile('./typedoc-pkg.config.mjs'))
+         {
+            Logger.verbose(`Loading config from path: './typedoc-pkg.config.mjs'`);
+            config = await loadConfig(path.resolve('./typedoc-pkg.config.mjs'));
+         }
+         break;
+
+      // Load specific config.
+      case 'string':
+      {
+         const configPath = path.resolve(opts.config);
+
+         if (!isFile(configPath)) { exit(`No config file available at: ${configPath}`); }
+
+         Logger.verbose(`Loading config from path: '${configPath}'`);
+         config = await loadConfig(configPath);
+         break;
+      }
+   }
+
+   // Apply any global command line options to overriding config file values.
+   if (isIterable(config))
+   {
+      for (const entry of config)
+      {
+         if (typeof loglevel === 'string') { entry.logLevel = loglevel; }
+      }
+   }
+   else if (isObject(config))
+   {
+      if (typeof loglevel === 'string') { config.logLevel = opts.loglevel; }
+   }
 
    return config;
 }
