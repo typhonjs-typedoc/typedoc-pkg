834e31345e210bae0b55a573b5d8806c4f048228
typhonrt <support@typhonjs.io>
2023-12-25 04:07:48 -0800

    Add private types.

---
+++ src/generator/types.d.ts
@@ -0,0 +1,69 @@
+import type { TypeDocOptions }   from 'typedoc';
+import type { CompilerOptions }  from 'typescript';
+
+// Any types defined in `types.d.ts` file are not included in public package types.
+
+/**
+ * Internal TypeDoc configuration.
+ */
+export type PkgTypeDocConfig = {
+   /**
+    * Typescript compiler options.
+    */
+   compilerOptions: CompilerOptions;
+
+   /**
+    * Current Working Directory.
+    */
+   cwd: string;
+
+   /**
+    * Modify navigation module paths to be flat or compact singular paths.
+    */
+   dmtNavStyle?: 'compact' | 'flat';
+
+   /**
+    * Module name substitution.
+    */
+   dmtModuleNames: Record<string, string>;
+
+   /**
+    * All entry point files to include in doc generation.
+    */
+   entryPoints: string[];
+
+   /**
+    * True if all entry points are Typescript declarations.
+    */
+   entryPointsDTS: boolean;
+
+   /**
+    * Indicates that the entry point files are from `package.json`.
+    */
+   fromPackage: boolean;
+
+   /**
+    * When true indicates that custom compiler options were loaded from `tsconfig` option.
+    */
+   hasCompilerOptions: boolean;
+
+   /**
+    * All API link plugins to load.
+    */
+   linkPlugins: Iterable<string>;
+
+   /**
+    * Documentation output directory.
+    */
+   output: string;
+
+   /**
+    * Options loaded from `typedocPath` option.
+    */
+   typedocJSON: Partial<TypeDocOptions>;
+
+   /**
+    * Direct TypeDoc options to set.
+    */
+   typedocObject?: Partial<TypeDocOptions>;
+};
